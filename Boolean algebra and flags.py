b138ea790057889766c8c339aa048c9dc2145be6
Adya Krishna <adya-krishna@syk.fi>
2026-08-26 12:50:58 +0300

    *26/08/26 changes

--- Boolean algebra and flags.py
+++ Boolean algebra and flags.py
@@ -1,8 +1,52 @@
 #EXAMPLE 2
+#for part 1
+#h = int(input("give a year: "))
+
+#leap = False
+
+#if h % 4 == 0:
+#    leap = True
+
+#if h % 100 == 0:
+#    leap= False
+
+#if h % 400 == 0:
+#    leap = True
+
+#print(leap)
+
 #for part 2
-h = int(input("give a year: "))
-if h % 4 == 0:
-    print("is a leap year")
-else:
-    print("isn't a leap year")
-# for part 1
+#print((h%400==0) or (h % 4 == 0 and not h% 100 == 0))
+
+#EXAMPLE 3
+#while not (1<= (v := int(input("what is the day of the week(1-7)"))) <= 7):
+#    print('please provide a valid day of week')
+
+#while (vacation := input('is James on vacation (yes/no): ')) != 'yes' and vacation != 'no':
+#    pass
+
+#print(vacation == 'yes' or v > 5)
+
+#HW1
+
+#n = input('is the sun shining: ')
+#b = int(input('current time (0-23)'))
+#value = False
+#if n:='yes' and b in range(10,17):
+#    value = True
+
+#print(value)
+
+#HW2
+
+z = int(input('give integer: '))
+x = int(input('give integer again: '))
+
+
+
+
+    
+
+
+
+
