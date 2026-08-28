cfaafeece44c6bf1ca4902d603b3abf5e48a85d2
Adya Krishna <adya-krishna@syk.fi>
2026-08-28 08:33:14 +0300

    *28/08/26 changes

--- Boolean algebra and flags.py
+++ Boolean algebra and flags.py
@@ -39,8 +39,65 @@
 
 #HW2
 
-z = int(input('give integer: '))
-x = int(input('give integer again: '))
+#z = int(input('give integer: '))
+#x = int(input('give integer again: '))
+
+#while (x % 10 == z % 10):
+#    print(True)
+#    break
+#else: 
+#   print(False)
+
+#HW3
+#rhs= int(input('enter a positive integer: '))
+#n = 0
+#while rhs > 0 and n**3 - 10*n**2 < rhs:
+#    n += 1
+#print(f'{n}')
+#print(f'{n**3-10*n**2}')
+
+#HW4
+#a = int(input('give positive integer: '))
+#b = int(input('give another positive integer: '))
+
+#n = 1
+
+#while not (n % a == 0) and not(n % b == 0):
+#   n += 1
+
+#print(n)
+
+#HW6
+
+#y = int(input('give a positive integer: '))
+
+#v = 2
+
+#while y % v != 0:
+#    v += 1
+#    if v == y:
+#        v = y
+
+#rint(v)
+
+#HW8
+#for y in range(2,100):
+ #   for v in range(2,y):
+ #       if y % v == 0:
+ #           break
+ #   else:
+ #
+ #        print(y, end=' ')
+
+new = []
+for y in range(2,100):
+        for v in range(2,y):
+            if len(new) <= 100 and y % v == 0:
+                     new.append(y)
+                     
+        else:
+            print(new, end=' ')
+
 
 
 
